12345a13d52dfe7964614d87683bbbd489f62617
ayl0002 <57423347+ayl0002@users.noreply.github.com>
2019-12-19 01:18:06 -0600

    Add files via upload

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,64 @@
-#filler
+import os
+import csv
+
+canName = []
+totalVote = 0
+canVote = []
+canCount = 0
+canDict = {}
+
+def rowcount(csvreader):
+    global totalVote
+    global canName
+    global canCount
+    for row in csvreader:
+        totalVote += 1
+        if row[2] not in canName:
+            canName.append(row[2])
+            canCount += 1
+            canDict = {canName : 0}
+
+        
+
+""" def votecount(csvreader):
+    global canName
+    for row in csvreader:
+        for i in canName:
+            if row[2] == i: """
+                
+   
+
+csvpath = os.path.join("Resources\election_data.csv")
+
+with open (csvpath,newline ='') as csvElect:
+
+    csvreader = csv.reader(csvElect,delimiter =',')
+
+    csv_header = next(csvreader)
+
+    rowcount(csvreader)
+
+with open (csvpath,newline ='') as csvElect:
+
+    csvreader = csv.reader(csvElect,delimiter =',')
+
+    csv_header = next(csvreader)
+    
+    votecount(csvreader)
+    
+print(totalVote)
+print(canName)
+print(canVote)
+    
+  
+
+
+
+ 
+
+
+        
+
+
+   
+  
