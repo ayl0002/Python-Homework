c1c5d4228f1bdb58fd2e2a568087dd65fe1c86b0
ayl0002 <57423347+ayl0002@users.noreply.github.com>
2019-12-19 20:06:58 -0600

    Add files via upload

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,7 +2,11 @@ import os
 import csv
 
 totalVote = 0
+canName = []
+canVote = []
 canDict = {}
+canPercent = {}
+canWin = 0
 
 def rowcount(csvreader):
     global totalVote
@@ -10,21 +14,30 @@ def rowcount(csvreader):
     for row in csvreader:
         totalVote += 1
         if row[2] not in canDict:
-            #canName.append(row[2])
+            canName.append(row[2])
             canDict.update({row[2]:0})
             
             
 def votecount(csvreader):
     global canDict
-    global voteCount
+    global canVote
     for row in csvreader:
-           for i in canDict:
-                if row[2] == i:
-                    canDict.update({i:canDict[i]+1})
-                    
+         for i in canDict:
+            if row[2] == i:
+                canDict.update({i:canDict[i]+1})
 
                 
-   
+
+                 
+def votepercent(csvreader):
+    global canDict
+    global canPercent
+    global totalVote
+    global canWin
+    print(totalVote)
+    for j in canDict:
+        canPercent.update({j:round((canDict[j]/totalVote)*100,2)})
+
 
 csvpath = os.path.join("Resources\election_data.csv")
 
@@ -43,11 +56,19 @@ with open (csvpath,newline ='') as csvElect:
     csv_header = next(csvreader)
     
     votecount(csvreader)
+
+with open (csvpath,newline ='') as csvElect:
+
+    csvreader = csv.reader(csvElect,delimiter =',')
+
+    csv_header = next(csvreader)
     
+    votepercent(csvreader)
+
 print(totalVote)
 print(canDict)
-#print(canVote)
-    
+print(canPercent)
+print(canWin)    
   
 
 
